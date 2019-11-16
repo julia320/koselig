a8abefda6310dabfcdeace739aa7062c31288ae5
suttonhowell <sarahhowell.school@gmail.com>
2019-11-16 16:59:30 -0500

    add lyric activity page
    Co-authored-by: Julia Bristow <julia_bristow@gwu.edu>

--- app/src/main/java/com/example/koselig/LyricActivity.kt
+++ app/src/main/java/com/example/koselig/LyricActivity.kt
@@ -1,16 +1,12 @@
 package com.example.koselig
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LyricActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, LyricActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_lyric)
     }
 }
