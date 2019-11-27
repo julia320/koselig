f36ec6c45f8a6d4f2d012b83fe3faca9cf3a35d0
Julia Bristow <julia_bristow@gwu.edu>
2019-11-26 23:31:38 -0500

    translate text into Spanish (test)
    Co-authored-by: Sutton Howell <sarahhowell.school@gmail.com>

--- app/src/main/java/com/example/koselig/LyricActivity.kt
+++ app/src/main/java/com/example/koselig/LyricActivity.kt
@@ -1,16 +1,23 @@
 package com.example.koselig
 
 import android.os.Bundle
+import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.TextView
 import android.widget.Toast
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.translate.Translate
+import com.google.cloud.translate.TranslateOptions
+import java.io.IOException
 
 class LyricActivity : AppCompatActivity() {
 
     private val lyricManager: LyricsManager = LyricsManager()
-
     private lateinit var recyclerView: RecyclerView
+    private var translate: Translate? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +31,40 @@ class LyricActivity : AppCompatActivity() {
         val inputTitle = intent.getStringExtra("titleInput")
         val inputArtist = intent.getStringExtra("artistInput")
 
+
+        // Get translation
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        try {
+            resources.openRawResource(R.raw.credentials).use { `is` ->
+                val myCredentials = GoogleCredentials.fromStream(`is`)
+                val translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build()
+                translate = translateOptions.service
+            }
+
+        }
+        catch (ioe: IOException) {
+            ioe.printStackTrace()
+        }
+
+
         //get path
         lyricManager.retrieveSongLyrics(
             titleInput = inputTitle,
             artistInput = inputArtist,
             successCallback = { lyrics ->
                 runOnUiThread {
+                    // Get the translation
+                    val translation = translate!!.translate (
+                        lyrics,
+                        Translate.TranslateOption.targetLanguage("es"),
+                        Translate.TranslateOption.model("base")
+                    )
                     // Create the adapter and assign it to the RecyclerView
-                    recyclerView.adapter = LyricAdapter(lyrics)
+
+                    recyclerView.adapter = LyricAdapter(translation)
                     if (lyrics.isEmpty()){
-                        Toast.makeText(this@LyricActivity, "The lyrics could not found or do not exist in the database", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this@LyricActivity, R.string.lyric_error, Toast.LENGTH_LONG).show()
                     }
                 }
             },
@@ -46,6 +77,6 @@ class LyricActivity : AppCompatActivity() {
         )
 
 
-
     }
 }
+
