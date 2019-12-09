926381fa5c009a5dfbadb5b53611e0a8aa633265
Julia Bristow <julia_bristow@gwu.edu>
2019-12-09 16:57:31 -0500

    sign in works

--- app/src/main/java/com/example/koselig/SignUpActivity.kt
+++ app/src/main/java/com/example/koselig/SignUpActivity.kt
@@ -1,11 +1,16 @@
 package com.example.koselig
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseUser
 
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -22,9 +27,29 @@ class SignUpActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
-        //val username = findViewById(R.id.username)
-        //val password = findViewById(R.id.password)
+        val username = findViewById(R.id.username) as EditText
+        val password = findViewById(R.id.password) as EditText
         signUp = findViewById(R.id.signUp)
+
+        signUp.setOnClickListener {
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+            firebaseAuth
+                .createUserWithEmailAndPassword(inputtedUsername, inputtedPassword)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val currentUser: FirebaseUser? = firebaseAuth.currentUser
+                        val email = currentUser?.email
+                        Toast.makeText(this, "Registered as $email", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, SignUpActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        val exception = task.exception
+                        Toast.makeText(this, "Registration failed: $exception", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+        }
     }
 
 }
